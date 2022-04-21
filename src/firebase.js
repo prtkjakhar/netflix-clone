a6097c778b5f0a6a14a6cc91b2b53e52d02b26e1
Prateek Jakhar <prtkjakhar@gmail.com>
2022-04-21 23:25:40 +0530

    new firebase files

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhkzMNxuD06IT6DbhNDXq1qGKhPl4RTsQ",
-    authDomain: "netflix-clone-7623f.firebaseapp.com",
-    projectId: "netflix-clone-7623f",
-    storageBucket: "netflix-clone-7623f.appspot.com",
-    messagingSenderId: "529143036680",
-    appId: "1:529143036680:web:ec2bfe255f3e0d679b0abd",
-    measurementId: "G-V27T19K37V"
-  };
+  apiKey: "AIzaSyB1Skw7ZZkufPNydKC4spGt1c14X-Fdu84",
+  authDomain: "netflix-clone-87b50.firebaseapp.com",
+  projectId: "netflix-clone-87b50",
+  storageBucket: "netflix-clone-87b50.appspot.com",
+  messagingSenderId: "343155117772",
+  appId: "1:343155117772:web:124cf72c4774b0e0da7c69",
+  measurementId: "G-H0YFJJTTVW"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
